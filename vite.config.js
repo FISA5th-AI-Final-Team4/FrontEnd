2b1520f3982ef3dadab633f4f2f0df90e90a2c85
ljh4770 <ljh4770@naver.com>
2025-10-24 21:51:07 +0900

    Revert "Merge pull request #12 from FISA5th-AI-Final-Team4/dev"
    This reverts commit 3798ad5700ad704f3e1234265cda1222042fa5bf, reversing
    changes made to 6dfa291567ca15ad70a130543fbeae9b07cdf045.

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return defineConfig({
-    plugins: [
-      react({
-        babel: {
-          plugins: [['babel-plugin-react-compiler']],
-        },
-      }),
-    ],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-        },
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
       },
-    },
-  });
-};
+    }),
+  ],
+})
